c12d7667442c514f52ecb969e00b89aef7482cf5
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-11-04 14:13:35 +0300

    add prototype for game v3

--- src/component/HardLevel/index.js
+++ src/component/HardLevel/index.js
@@ -13,53 +13,45 @@ export default function HardLevel() {
   const [real, setReal] = useState([
     {
       id: 1,
-      img: "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=",
-      hIamge:
+      back: "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=",
+      front:
         "https://pe-images.s3.amazonaws.com/basics/cc/image-size-resolution/resize-images-for-print/image-cropped-8x10.jpg",
     },
     {
       id: 2,
-      img: "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=",
-      hIamge: "http://webmeup.com/upload/blog/lead-image-105.png",
+      back: "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=",
+      front: "http://webmeup.com/upload/blog/lead-image-105.png",
     },
     {
       id: 3,
-      img: "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=",
-      hIamge:
+      back: "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=",
+      front:
         "https://pe-images.s3.amazonaws.com/basics/cc/image-size-resolution/resize-images-for-print/image-cropped-8x10.jpg",
     },
     {
       id: 4,
-      img: "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=",
-      hIamge: "http://webmeup.com/upload/blog/lead-image-105.png",
+      back: "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=",
+      front: "http://webmeup.com/upload/blog/lead-image-105.png",
     },
   ]);
 
   const [finishedCardNum, setFinishedCardNum] = useState(0);
-
-  const [flexNum, setFlexNum] = useState([0]);
+  const [flipNum, setFlipNum] = useState([0]);
 
   const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-  function wait(ms) {
-    let start = new Date().getTime();
-    let end = start;
-    while (end < start + ms) {
-      end = new Date().getTime();
-    }
-  }
 
   const check = async (id) => {
-    let finishCard =
+    let frontCard =
       "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
     let backCard =
       "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=";
 
-    if (real[id - 1].img !== finishCard)
-      if (real[id - 1].img === backCard) {
+    if (real[id - 1].back !== frontCard)
+      if (real[id - 1].back === backCard) {
         //count number of cards to check if the same or not
         //1-change count to each card
         const newCh = ch.map((item) => {
-          if (real[id - 1].hIamge === item.img) {
+          if (real[id - 1].front === item.img) {
             item.count++;
           }
           return item;
@@ -68,31 +60,26 @@ export default function HardLevel() {
 
         //2-use find to return count
         let countCard = ch.find((item) => {
-          return real[id - 1].hIamge === item.img;
+          return real[id - 1].front === item.img;
         });
 
         const newReal = real.map((item) => {
           if (item.id === id) {
-            item.img = item.hIamge;
+            item.back = item.front;
           }
           return item;
         });
         setReal(newReal);
 
         await delay(1000);
-        //wait(1000);
-
-        console.log("Waited 1s");
-
-        //console.log(countCard.count);
 
         //3- cheack if is equals or not
         //begin
         if (countCard.count === 2) {
-          //1- change image to finishCard
+          //1- change image to frontCard
           const newReal = real.map((item) => {
-            if (item.hIamge === real[id - 1].hIamge) {
-              item.img = finishCard;
+            if (item.back === real[id - 1].front) {
+              item.back = frontCard;
             }
             return item;
           });
@@ -104,15 +91,17 @@ export default function HardLevel() {
         }
         //end
 
-        flexNum[0]++;
-        console.log(flexNum[0]);
-        if (flexNum[0] === 2 && countCard.count !== 2) {
+        // check how many card are filp
+        //beagin
+        flipNum[0]++;
+
+        if (flipNum[0] === 2 && countCard.count !== 2) {
           const newReal = real.map((item) => {
-            item.img =
+            item.back =
               "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=";
             return item;
           });
-          flexNum[0] = 0;
+          flipNum[0] = 0;
           setReal(newReal);
           //set all count to 0
           const newCh2 = ch.map((item) => {
@@ -121,46 +110,41 @@ export default function HardLevel() {
           });
 
           setCh(newCh2);
-          console.log(newCh2);
-        } else if (flexNum[0] === 2) flexNum[0] = 0;
-        setFlexNum(flexNum);
+        } else if (flipNum[0] === 2) flipNum[0] = 0;
+        setFlipNum(flipNum);
+        //finish check from filpNum
 
         //finish
       } else {
         const newReal = real.map((item) => {
           if (item.id === id) {
-            item.img =
+            item.back =
               "https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk=";
           }
           return item;
         });
-
         setReal(newReal);
       }
-
-    // if (a === 0) {
-    // } else if (a === 1) {
-    // } else {
-    // }
   };
 
   return (
     <div className="main">
-      {real.map((item, i) => {
-        return (
-          <div className="cardContiner">
-            <div className="card">
+      <div className="cardContiner">
+        {real.map((item, i) => {
+          return (
+            <div className="card" key={i}>
               <img
-                src={item.img}
+                src={item.back}
                 className="backImg"
                 onClick={() => {
                   check(i + 1);
                 }}
+                alt="card"
               />
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
 
       <h1>{finishedCardNum} </h1>
     </div>
